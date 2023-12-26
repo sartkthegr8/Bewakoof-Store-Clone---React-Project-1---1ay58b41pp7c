cf0678451ed9114d4138ed0cf2c2980d7caf051e
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-12-26 18:06:18 +0530

    cc

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 const Header = () => {
+
+  const [logBtn,setLogBtn]=useState("login");
+  // let logBtn = "login"; 
+  const togglelogBtn = ()=>{
+    if(logBtn==="login"?setLogBtn("logout"):setLogBtn("login"));
+  }
+  
   return (
     <div className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex items-center justify-between">
@@ -13,6 +22,12 @@ const Header = () => {
             <li className="cursor-pointer">About Us</li>
             <li className="cursor-pointer">Contact Us</li>
             <li className="cursor-pointer">Cart</li>
+            <button 
+             className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+             onClick={togglelogBtn}
+            >
+              {logBtn}
+            </button>
           </ul>
         </div>
       </div>
